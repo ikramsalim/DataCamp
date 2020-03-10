e4784c3717f6e2f42c80af789cebee868e1edbed
Ikram <ikramomar998@gmail.com>
2020-03-10 09:12:05 -0600

    The single bracket version gives a Pandas Series, the double bracket version gives a Pandas DataFrame.

--- 02-intermediate-python/2-dictionaries&pandas/dictionary-to-dataframe(1).py
+++ 02-intermediate-python/2-dictionaries&pandas/dictionary-to-dataframe(1).py
@@ -2,10 +2,6 @@
 names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
 dr =  [True, False, False, False, True, True, True]
 cpc = [809, 731, 588, 18, 200, 70, 45]
-
-
-
-
 # Create dictionary my_dict with three key:value pairs: my_dict
 
 
