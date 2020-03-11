e51841e81c8e2e15f2f9cfb7016c3049b5cc0a99
Ikram <ikramomar998@gmail.com>
2020-03-10 23:58:19 -0600

    run the commented code using python shell

--- 02-intermediate-python/2-dictionaries&pandas/loc-and-iloc.py
+++ 02-intermediate-python/2-dictionaries&pandas/loc-and-iloc.py
@@ -1,3 +1,13 @@
+"""
+Use loc or iloc to select the observation corresponding to Japan as a Series. The label of this row is JPN, the index is 2.
+ Make sure to print the resulting Series.
+Use loc or iloc to select the observations for Australia and Egypt as a DataFrame.
+You can find out about the labels/indexes of these rows by inspecting cars in the IPython Shell.
+Make sure to print the resulting DataFrame.
+
+"""
+
+
 # Import cars data
 import pandas as pd
 cars = pd.read_csv('cars.csv', index_col = 0)
@@ -7,3 +17,12 @@ print(cars.iloc[2])
 
 # Print out observations for Australia and Egypt
 print(cars.loc[[ 'AUS', 'EG']])
+
+# cars.loc['RU']
+# cars.iloc[4]
+#
+# cars.loc[['RU']]
+# cars.iloc[[4]]
+#
+# cars.loc[['RU', 'AUS']]
+# cars.iloc[[4, 1]]
