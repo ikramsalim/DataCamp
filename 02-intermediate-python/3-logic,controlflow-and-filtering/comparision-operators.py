fdbb369f898dcc8be12d9bd469aaa43a363f90ab
Ikram <ikramomar998@gmail.com>
2020-03-11 01:10:32 -0600

    Wrap a print() function around them to see the output.

---
+++ 02-intermediate-python/3-logic,controlflow-and-filtering/comparision-operators.py
@@ -0,0 +1,24 @@
+"""
+In the editor on the right, write code to see if True equals False.
+Write Python code to check if -5 * 15 is not equal to 75.
+Ask Python whether the strings "pyscript" and "PyScript" are equal.
+What happens if you compare booleans and integers? Write code to see if True and 1 are equal.
+"""
+# Comparison of booleans
+print(True == False)
+# Comparison of integers
+
+print(-5 * 15 !=  75)
+# Comparison of strings
+print("pyscript"== "PyScript")
+
+# Compare a boolean with an integer
+print(True == 1 )
+
+
+"""try in iPython Shell
+2 == (1 + 1)
+"intermediate" != "python"
+True != False
+"Python" != "python"
+"""
