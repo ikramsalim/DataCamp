8aef23324ea1dc242bfdff31ee05a2bf0036f9be
Ikram <ikramomar998@gmail.com>
2020-03-11 04:12:27 -0600

    for-loop that goes through each sublist of house

---
+++ 02-intermediate-python/4-loops/loop-over-lists-of-lists.py
@@ -0,0 +1,12 @@
+"""Write a for loop that goes through each sublist of house and prints out the x is y sqm, where x is the name of the room
+ and y is the area of the room."""
+# house list of lists
+house = [["hallway", 11.25],
+         ["kitchen", 18.0],
+         ["living room", 20.0],
+         ["bedroom", 10.75],
+         ["bathroom", 9.50]]
+
+# Build a for loop from scratch
+for room, area in (house):
+    print("the " + str(room) + " is " + str(area) + " sqm")
