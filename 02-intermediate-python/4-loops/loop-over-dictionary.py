b8a2750c6ded3572855995155af17263a1ffee71
Ikram <ikramomar998@gmail.com>
2020-03-11 04:17:56 -0600

    Writing a loop to iterate over a dictionary follows the format::::::::: for key, value in world.items() :     print(key + " -- " + str(value))

---
+++ 02-intermediate-python/4-loops/loop-over-dictionary.py
@@ -0,0 +1,12 @@
+"""
+Write a for loop that goes through each key:value pair of europe. On each iteration, "the capital of x is y" should be printed out, where x is the key and y is the value of the pair.
+
+
+"""
+# Definition of dictionary
+europe = {'spain': 'madrid', 'france': 'paris', 'germany': 'berlin',
+          'norway': 'oslo', 'italy': 'rome', 'poland': 'warsaw', 'austria': 'vienna'}
+
+# Iterate over europe
+for key, value in europe.items():
+    print("the capital of " + key + " is " + value)
