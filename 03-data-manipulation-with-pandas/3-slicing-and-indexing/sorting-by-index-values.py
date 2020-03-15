046abde672853f378eed50b5682f1b48db5713aa
Ikram <ikramomar998@gmail.com>
2020-03-15 10:44:15 -0600

    You can change the order of the rows in a DataFrame by calling .sort_values(). It's also useful to be able to sort by elements in the index. For this, you need to use .sort_index().

---
+++ 03-data-manipulation-with-pandas/3-slicing-and-indexing/sorting-by-index-values.py
@@ -0,0 +1,16 @@
+"""
+Instructions
+
+Sort temperatures_ind by the index values.
+Sort temperatures_ind by the index values at the "city" level.
+Sort temperatures_ind by ascending country then descending city.
+"""
+# Sort temperatures_ind by index values
+print(temperatures_ind.sort_index())
+
+# Sort temperatures_ind by index values at the city level
+print(temperatures_ind.sort_index(level="city"))
+
+# Sort temperatures_ind by country then descending city
+print(temperatures_ind.sort_index(level=["country",
+"city"], ascending=[True, False]))
