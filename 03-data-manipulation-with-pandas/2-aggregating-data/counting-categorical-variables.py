b351399d9ec95059bdf37f6da12aa77804be5ef1
Ikram <ikramomar998@gmail.com>
2020-03-15 08:09:14 -0600

    .value_counts()

--- 03-data-manipulation-with-pandas/2-aggregating-data/counting-categorical-variables.py
+++ 03-data-manipulation-with-pandas/2-aggregating-data/counting-categorical-variables.py
@@ -1,3 +1,9 @@
+"""
+Count the number of stores of each type.
+Count the proportion of stores of each type.
+Count the number of departments of each type, sorting the counts in descending order.
+Count the proportion of departments of each type, sorting the proportions in descending order.
+"""
 # Count the number of stores of each type
 store_counts = stores["type"].value_counts()
 print(store_counts)
@@ -13,3 +19,9 @@ print(dept_counts_sorted)
 # Get the proportion of departments of each type and sort
 dept_props_sorted = departments["department"].value_counts(sort=True, normalize=True)
 print(dept_props_sorted)
+
+"""
+store_counts = stores["type"].value_counts()
+store_props = stores["type"].value_counts(normalize=True)
+dept_props_sorted = departments["department"].value_counts(sort=True, normalize=True)
+"""
