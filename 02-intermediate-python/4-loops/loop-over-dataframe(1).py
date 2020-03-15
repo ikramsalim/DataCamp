be7aa2fb488371ef3b8a1af3a55d78fb6c8e94ba
Ikram <ikramomar998@gmail.com>
2020-03-15 08:56:50 -0600

    Print the median,mean of the weekly_sales column of the "sales" DataFrame.

--- 02-intermediate-python/4-loops/loop-over-dataframe(1).py
+++ 02-intermediate-python/4-loops/loop-over-dataframe(1).py
@@ -1,8 +1,7 @@
 """
-Write a for loop that iterates over the rows of cars and on each iteration perform two print() calls: one to print out
- the row label and one to print out all of the rows contents.
+Write a for loop that iterates over the rows of cars and on each iteration perform two print() calls: one to print out the row label and one to print out all of the rows contents.
 """
-#Import cars data
+# Import cars data
 import pandas as pd
 cars = pd.read_csv('cars.csv', index_col = 0)
 
